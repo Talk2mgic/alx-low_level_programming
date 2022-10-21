1052cf503737fae9fc7e6a2020e67657e251349a
Talk2mgic <talk4mojeed@yahoo.com>
2022-10-21 11:09:50 -0700

    1 is digit

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -2,11 +2,11 @@
 #include "main.h"
 
 /**
- * _putchar - write the character c to stdout
+ * _putchar - writes the character c to stdout
  * @c - The character to print
  * Return: on success 1 else -1
  */
 int _putchar(char c)
 {
-	return(write(1, &c, -1));
+	return (write(1, &c, 1));
 }
