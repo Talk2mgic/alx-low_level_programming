3cc63746cfef11b77082ab01f40c19d3090e07ea
Talk2mgic <talk4mojeed@yahoo.com>
2022-10-21 12:20:42 -0700

    4 print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print number 0 to 9
+ * without 2 and 4
+ * Return: Always 0
+ */
+void print_most_numbers(void)
+{
+	int m = 0;
+
+	while (m < 10)
+	{
+		if (m != 2 && m != 4)
+			_putchar(m + '0');
+		m++;
+	}
+	_putchar('\n');
+}
