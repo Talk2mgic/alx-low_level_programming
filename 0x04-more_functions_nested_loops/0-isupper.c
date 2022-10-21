2d7c42144910a4c9259fa3f05e31aa13d6de093e
Talk2mgic <talk4mojeed@yahoo.com>
2022-10-21 07:29:14 -0700

    0 is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _isupper - checks if the character is uppercase
+ * _isupper - checks if a character is uppercase
  * @c: Variable text
  * Return: Always 0
  */
-int _isupper(char c)
+int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 	{
