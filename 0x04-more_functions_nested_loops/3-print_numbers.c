248573b94b831b80b36924c5c2d4774b4b61904f
Talk2mgic <talk4mojeed@yahoo.com>
2022-10-21 05:27:10 -0700

    3 print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print from 0 to 9
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	m = 0;
+
+	while (m < 10)
+		_putchar(a++ + '0');
+
+	_putchar('\n');
+}
