40986c2ce5b237035f04cc9a3ddcc6d7f4bff885
Talk2mgic <talk4mojeed@yahoo.com>
2022-10-21 16:26:35 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,11 +14,14 @@ int main(void)
 		printf(" ");
 		if (n % 3 == 0)
 			printf("Fizz");
+
 		if (n % 5 == 0)
 			printf("Buzz");
+
 		if (n % 3 != 0 && n % 5 != 0)
 			printf("%d", n);
 	}
 	printf("\n");
+
 	return (0);
 }
