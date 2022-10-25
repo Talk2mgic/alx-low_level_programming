fe615ed37711116d7676446f75d15602dd359cce
Talk2mgic <talk4mojeed@yahoo.com>
2022-10-25 16:26:57 -0700

    print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_rev -prints a string in reverser
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+	int a = 0;
+
+	while (s[a] != '\n')
+		a++
+			for (a = a - 1; q >= 0; a--)
+				_putchar(s[a]);
+	_putchar('\n');
+}
