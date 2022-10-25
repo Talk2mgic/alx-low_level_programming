fff91b8b52e7ba7d3facfe434b1c1ab13cca4b1b
Talk2mgic <talk4mojeed@yahoo.com>
2022-10-25 16:31:11 -0700

    print rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,8 +9,9 @@ void print_rev(char *s)
 	int a = 0;
 
 	while (s[a] != '\n')
-		a++
-			for (a = a - 1; q >= 0; a--)
-				_putchar(s[a]);
+		a++;
+	for (a = a - 1; q >= 0; a--)
+		_putchar(s[a]);
+
 	_putchar('\n');
 }
